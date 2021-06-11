5c31ee466ee58927d35ac7c577722af4b3c6ecf0
Mauricio Lima <mauricio.lima@ibm.com>
2021-06-10 22:35:55 -0300

    Improved person creation funcionality

--- personapi/src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -1,9 +1,15 @@
 package one.digitalinnovation.personapi;
 
+import java.beans.JavaBean;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
+@Configuration
 public class PersonapiApplication {
 
 	public static void main(String[] args) {
--- personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,5 +1,7 @@
 package one.digitalinnovation.personapi.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,26 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinnovation.personapi.entity.Person;
-import one.digitalinnovation.personapi.service.PersonService;
+import one.digitalinnovation.personapi.services.PersonService;
 
 @RestController
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
 	private PersonService personService;
 	
-	@Autowired
-	public PersonController(PersonService personService) {
-		this.personService = personService;
-	}
-
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public MessageResponseDTO createPerson(@RequestBody Person person) {
+	public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
 
-		return personService.createPerson(person);
+		return personService.create(personDTO);
 	}
 }
 
